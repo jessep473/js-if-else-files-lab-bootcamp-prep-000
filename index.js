d2c20b33cd85ab6889996c572bd2c341a9537188
Jesse Peterson <jessep473@gmail.com>
2018-10-02 20:32:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ else {
   console.log('Name is incorrect')
 }
 
+if (height === "74") {
+  console.log('Height Is Correct')
+}
+
+
+else {
+  console.log('Height is incorrect')
+}
+
 // Don't worry about this
 module.exports = { name, height
 }
